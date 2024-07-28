5b2b7ea71a068935f2bf84ae5192eab58c364732
B CHAITANYA REDDY <158465498+BChaitanyaReddy895@users.noreply.github.com>
2024-07-28 21:05:53 +0530

    To_do_list.py

--- To_do_list.py
+++ To_do_list.py
@@ -1,27 +1,31 @@
+#initialising the empty list for storing the tasks
 task = []
 print("To-do List by B Chaitanya Reddy")
 print("Follow the below instructions for successful usage of the to-do list\n")
 print("select any of the choice based on your requirement\n")
+# instructions to be followed for successful usage of To-do list
 print("For adding the tasks, enter your choice as 1\n")
 print("For viewing the entered tasks, enter your choice as 2\n")
 print("For updating the tasks, enter your choice as 3\n")
 print("For appending the new tasks, enter your choice as 4\n")
 print("For removing tasks, enter your choice as 5\n")
 print("For exit, enter your choice as 6\n")
-
+# initialising the while loop until the user desires to exit the program
 while True:
-    user_choice=int(input("Enter your choice:"))
-    
+    user_choice=int(input("Enter your choice:"))#accepting the user's choice
+    # initialising the if condition based on user's choice for adding the tasks into the to-do list
     if user_choice==1:
         n=int(input("Enter the number of tasks:"))
         for i in range(n):
             task.append(input(f"Task {i+1}:"))
+    # initialising the elif condition based on user's choice to display the tasks in the to-do list
     elif user_choice==2:
         if task:
             for i, t in enumerate(task, 1):
                 print(f"Task {i}: {t}")
         else:
             print("No tasks available.\n")
+    # initialising the elif condition based on user's choice for updating the tasks in the to-do list
     elif user_choice==3:
         for i, t in enumerate(task, 1):
             print(f"{i}. {t}")
@@ -32,10 +36,12 @@ while True:
             print("Task updated successfully.")
         else:
             print("Invalid task number.")
+    # initialising the elif condition based on user's choice for appending the tasks into the to-do list
     elif user_choice==4:
         new_task=input("Enter the name of the task to append: ")
         task.append(new_task)
         print("Task added successfully.")
+    # initialising the elif condition based on user's choice for removing the tasks from the to-do list
     elif user_choice==5:
         for i, t in enumerate(task, 1):
             print(f"{i}. {t}")
@@ -45,7 +51,7 @@ while True:
             print(f"{removed_task} has been successfully removed from your to-do list.")
         else:
             print("Invalid task number.")
-
+    # initialising the if condition based on user's choice for closing the to-do list
     elif user_choice==6:
         print("closing your To-do List.")
         break
